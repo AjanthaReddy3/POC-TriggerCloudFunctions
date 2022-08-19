730d433367fb07794833a6ffd31710f9a6daf685
AjanthaReddy3 <46964192+AjanthaReddy3@users.noreply.github.com>
2022-08-19 15:03:18 -0400

    Update main.py

--- cloudfunction4/main.py
+++ cloudfunction4/main.py
@@ -3,13 +3,18 @@ import os
 
 def query_avgtemp(request):
   client = bigquery.Client()
-  avg_temp=os.environ.get('avg_temp')
+  env_avg_temp=os.environ.get('avg_temp')
   query=  """
   SELECT location,average_temperature,latest_measurement 
   FROM `pg-us-e-app-588206.sample_data.table2` 
-  WHERE average_temperature>=40 and latest_measurement>=current_date()-1900
+  WHERE average_temperature>=@average_temperature and latest_measurement>=current_date()-1900
   """
-  query_job = client.query(query)
+  job_config = bigquery.QueryJobConfig(
+    query_parameters=[
+      bigquery.ScalarQueryParameter("average_temperature", "INT64", env_avg_temp)
+    ]
+  )  
+  query_job = client.query(query, job_config=job_config)
   print("The Query response is:")
   for row in query_job:
     print("location={}, avg_temp={}, latest_measurement={}".format(row[0],row[1],row[2]))
